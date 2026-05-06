c1cd963f9c97a3ebf79b2fd374662082fec310fb
Amir <ziabariak@ornl.gov>
2026-05-05 22:53:42 -0400

    remove typo

--- examples/run_CAD_voxelize.py
+++ examples/run_CAD_voxelize.py
@@ -2,9 +2,9 @@
 from cad_voxelize import main
 
 main(
-    "/Users/3az/Library/CloudStorage/OneDrive-OakRidgeNationalLaboratory/2025-2026/Leidos/CUI_GAMMA-H Part.STL",
+    "FILENAME",
     resol=0.36,
     padding=25,
-    out_folder="/Users/3az/Library/CloudStorage/OneDrive-OakRidgeNationalLaboratory/2025-2026/Leidos/",
+    out_folder="OUTFOLDER",
     out_dtype="tiff", # "npy"
 )
